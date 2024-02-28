128a237f4649b83abd5b28c5758ebd6151c59ab4
kiboy19 <arielseptiadi3@gmail.com>
2024-02-28 14:22:55 +0700

    compress data

--- views/partials/daftar/header.php
+++ views/partials/daftar/header.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar - Halaman Pendaftaran Official Rajahemat</title>
+    <meta name="description" content="OFFICIAL RAJAHEMAT - Penyedia Layanan Boosting Social Media Termurah & Terpercaya Se-Indonesia">
+    <meta name=”robots” content="index, follow">
+    <meta name="keywords" content="OFFICIAL RAJAHEMAT, Official Rajahemat, Rajahemat, Smm Panel, Smm Panel Indonesia, Boosting Social Media, Boosting Social Media Termurah, Boosting Social Media Terbaik, Jasa Social Media, Jasa Social Media Termurah, Jasa Social Media Terbaik, Jasa Social Media Terpercaya, Buzzerpanel, Buzzer Panel, Lollipop SMM Panel, smm panel,smm panel indonesia, smm indonesia, panel murah, panel smm terpercaya, reseller panel smm indonesia, panel smm murah di indonesia, smm panel reseller, panel termurah indonesia, smm termurah indonesia, web smm no.1 di indonesia, reseller smm termurah indonesia, jasa penambah followers instagram, followers gratis ig, jasa likes instagram, kebutuhan sosial media. SMM Panel Indonesia yang menyediakan panel sosial media seperti Followers, Likes, Views, Subscriber, untuk beragam social media: Instagram Youtube, Facebook, Twitter dengan harga Termurah.
+Dengan bergabung bersama kami, Anda dapat menjadi penyedia jasa social media atau reseller social media seperti jasa penambah Followers, Likes, dll.
+Saat ini tersedia berbagai layanan untuk social media terpopuler seperti Instagram, Facebook, Twitter, Youtube, dll.">
+    <meta name="author" content="Muhamad Ariel Septiadi">
+    <link rel="icon" type="image/x-icon" href="img/rhnew.png">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/aos/aos.css" />
